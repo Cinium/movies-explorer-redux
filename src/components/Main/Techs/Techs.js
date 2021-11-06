a0eb1f7879201d42f88f7a5b6f62dd9fd8575825
Slavik <slava123101@gmail.com>
2021-11-06 16:07:55 +0500

    исправлено количество используемых технологий

--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -4,7 +4,7 @@ function Techs() {
     return (
         <div className="techs" id="techs">
             <h2 className="techs__title main__title">Технологии</h2>
-            <h3 className="techs__subtitle">7 технологий</h3>
+            <h3 className="techs__subtitle">8 технологий</h3>
             <p className="techs__text">
                 На курсе веб-разработки мы освоили технологии, которые
                 применили в дипломном проекте.
